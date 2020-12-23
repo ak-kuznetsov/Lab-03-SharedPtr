7832c7bef7f3ec094cd71912188ba35a856bf558
ak-kuznetsov <a.kuznetsov2701@mail.ru>
2020-12-23 22:51:30 +0300

    Add SharedPtr_Test.cpp

---
+++ tests/SharedPtr_Test.cpp
@@ -0,0 +1,43 @@
+// Copyright 2020 ak-kuznetsov <a.kuznetsov2701@mail.ru>
+
+#include <gtest/gtest.h>
+
+#include <SharedPtr.hpp>
+TEST(Example, EmptyTest) { EXPECT_TRUE(true); }
+
+TEST(SharedPtr_Test, Constructor_Test) {
+  int a{5};
+  SharedPtr<int> b{&a};
+  ASSERT_EQ(*b, a);
+}
+
+TEST(SharedPtr_Test, Rvalue) {
+  int a{9};
+  SharedPtr b(&a);
+  SharedPtr rvalue(std::move(b));
+  ASSERT_EQ(b.use_count(), 2);
+}
+
+TEST(SharedPtr_Test, Lvalue) {
+  int a{8};
+  SharedPtr b(&a);
+  SharedPtr lvalue(b);
+  ASSERT_EQ(b.use_count(), 2);
+}
+
+TEST(SharedPtr_Test, is_move_constructible){
+  ASSERT_EQ(std::is_move_constructible<SharedPtr<int>>::value,true);
+}
+
+TEST(SharedPtr_Test, is_move_assignable){
+  ASSERT_EQ(std::is_move_assignable<SharedPtr<int>>::value,true);
+}
+
+TEST(SharedPtr_Test, Swap){
+  int a{1};
+  int b{2};
+  SharedPtr<int> A(&a);
+  SharedPtr<int> B(&b);
+  B.swap(A);
+  ASSERT_EQ(*B, 1);
+}
